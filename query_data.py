474e8a04d98b599ede287538f2669ea19d5f2e7b
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-03-31 09:10:26 +0200

    Fixed according to pyright.

--- query_data.py
+++ query_data.py
@@ -1,13 +1,10 @@
 import argparse
-from dataclasses import dataclass
 from langchain.vectorstores.chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 
 from dotenv import load_dotenv
-
-import os
 
 load_dotenv()
 
@@ -32,7 +29,7 @@ def main():
     query_text = args.query_text
 
     # Prepare the DB.
-    embedding_function = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    embedding_function = OpenAIEmbeddings()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
@@ -41,16 +38,15 @@ def main():
         print(f"Unable to find matching results.")
         return
 
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _ in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
     print(prompt)
 
-    model = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"),
-                       model_name="gpt-3.5-turbo")
+    model = ChatOpenAI()
     response_text = model.invoke(prompt)
 
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    sources = [doc.metadata.get("source", None) for doc, _ in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
 
